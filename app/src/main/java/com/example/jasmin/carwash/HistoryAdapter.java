0a7472d7a6c596a6b5063767e6c8d924f238159f
SantillanaJasmin <jasminsantillana_cse@yahoo.com>
2017-02-04 15:00:57 +0800

    First commit

---
+++ app/src/main/java/com/example/jasmin/carwash/HistoryAdapter.java
@@ -0,0 +1,77 @@
+package com.example.jasmin.carwash;
+
+import android.content.Context;
+import android.support.v7.widget.PopupMenu;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Jasmin on 2/1/2017.
+ */
+public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.HistoryViewHolder> {
+
+    private ArrayList<History> historyList;
+    private Context appContext;
+    private OnItemClickListener mOnDetailClickListener, mOnDeleteClickListener;
+
+    public HistoryAdapter(Context context, ArrayList<History> list) {
+        this.appContext = context;
+        this.historyList = list;
+    }
+
+    public class HistoryViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvControlNumberLabel, tvControlNumberField, tvDateLabel, tvDateField;
+        View container;
+
+        public HistoryViewHolder(View itemView) {
+            super(itemView);
+
+            tvControlNumberLabel = (TextView) itemView.findViewById(R.id.tvControlNumLabel);
+            tvControlNumberField = (TextView) itemView.findViewById(R.id.tvControlNumField);
+            tvDateLabel = (TextView) itemView.findViewById(R.id.tvDateLabel);
+            tvDateField = (TextView) itemView.findViewById(R.id.tvDateField);
+
+            container = itemView.findViewById(R.id.container);
+        }
+    }
+
+//    public void setmOnDetailClickListener(OnItemClickListener m) {
+//        this.mOnDetailClickListener = m;
+//    }
+//
+//    public void setmOnDeleteClickListener(OnItemClickListener m) {
+//        this.mOnDeleteClickListener = m;
+//    }
+
+    @Override
+    public HistoryViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.history_item, parent, false);
+        return new HistoryViewHolder(v);
+    }
+
+    public interface OnItemClickListener {
+        public void onItemClick(int id);
+    }
+
+    @Override
+    public void onBindViewHolder(final HistoryViewHolder holder, int position) {
+        History history = historyList.get(position);
+
+        holder.tvControlNumberField.setText(history.getTrans_number());
+        holder.tvDateField.setText((CharSequence) history.getDate());
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/example/jasmin/carwash/ViewHistoryActivity.java
@@ -0,0 +1,209 @@
+package com.example.jasmin.carwash;
+
+import android.app.DialogFragment;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.database.Cursor;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+import java.util.concurrent.ExecutionException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class ViewHistoryActivity extends AppCompatActivity {
+
+    ArrayList<History> historyList;
+    HistoryAdapter historyAdapter;
+    RecyclerView rvHistory;
+
+    String sResult ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_history);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        historyList = new ArrayList<>();
+        rvHistory = (RecyclerView) findViewById(R.id.rvHistory);
+
+        GetHistoryAsyncTask getHistoryAsyncTask = new GetHistoryAsyncTask(this);
+        try {
+            String s = getHistoryAsyncTask.execute().get();
+            populateHistoryRecyclerView(s);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        //            Toast.makeText(this, "History Array Size: " + historyList.size(), Toast.LENGTH_LONG).show();
+        historyAdapter = new HistoryAdapter(getBaseContext(), historyList);
+        rvHistory.setAdapter(historyAdapter);
+        rvHistory.setLayoutManager(new LinearLayoutManager(getBaseContext()));
+    }
+
+    public void populateHistoryRecyclerView(String s) {
+        try {
+            JSONObject jsonObject = new JSONObject(s);
+
+            // Getting JSON Array node
+            JSONArray historyArray = jsonObject.getJSONArray("bookings");
+
+            for(int i = 0; i < historyArray.length(); i++) {
+                int control_number = 0;
+                String date = "";
+                Date control_date = null;
+
+                JSONObject history = historyArray.getJSONObject(i);
+
+                try {
+                    control_number = history.getInt("control_no");
+                    date = history.getString("schedule");
+
+                    TimeZone utc = TimeZone.getTimeZone("UTC");
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+                    sdf.setTimeZone(utc);
+                    GregorianCalendar cal = new GregorianCalendar(utc);
+                    cal.setTime(sdf.parse(date));
+                    control_date = cal.getTime();
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                    continue;
+                } finally {
+                    historyList.add(new History(control_number, control_date));
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    public class GetHistoryAsyncTask extends AsyncTask<Void, Void, String> {
+        Context mContext;
+        ProgressDialog pdLoading;
+
+        public GetHistoryAsyncTask(Context context) {
+            this.mContext = context;
+
+            pdLoading = new ProgressDialog(mContext);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            //this method will be running on UI thread
+            pdLoading.setMessage("\tLoading...");
+            pdLoading.setCancelable(false);
+            pdLoading.show();
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            String url = "http://192.168.2.52:3004/carwash/history/1";
+
+            OkHttpClient client = new OkHttpClient();
+
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+
+            Response response;
+            try {
+                response = client.newCall(request).execute();
+                sResult = response.body().string();
+//                return response.body().string();
+                return sResult;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            pdLoading.dismiss();
+
+//            ArrayList<History> historyList = new ArrayList<>();
+//            try {
+//                JSONObject object = new JSONObject(s);
+//                JSONArray historyArray = new JSONArray(object.getJSONArray("bookings"));
+//
+//                for(int i = 0; i < historyArray.length(); i++) {
+//                    JSONObject history = historyArray.getJSONObject(i);
+//
+//                    try {
+//                        int control_number = history.getInt("control_no");
+//                        String date = history.getString("schedule");
+//                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//                        Date birthDate = sdf.parse(date);
+//
+//                        historyList.add(new History(control_number, birthDate));
+//                    } catch (ParseException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//
+//            if (s!= null)
+//            {
+//
+//                Toast.makeText(mContext, sResult, Toast.LENGTH_LONG).show();
+//            }
+//            else
+//            {
+//
+//                Toast.makeText(mContext, "null", Toast.LENGTH_LONG).show();
+//            }
+
+//            Toast.makeText(mContext, historyList.size(), Toast.LENGTH_LONG).show();
+//            historyAdapter = new HistoryAdapter(getBaseContext(), historyList);
+//            rvHistory.setAdapter(historyAdapter);
+//            rvHistory.setLayoutManager(new LinearLayoutManager(getBaseContext()));
+        }
+    }
+}
